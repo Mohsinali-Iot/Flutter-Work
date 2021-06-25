7a0acdf1f07bece807a8106a8e5280962cc2a25a
mohsinali-Iot <mohsinali481997@gmail.com>
2021-06-25 11:51:29 +0500

    drawer

--- lec3/lib/home.dart
+++ lec3/lib/home.dart
@@ -118,20 +118,73 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  var lists=["Ali","Saad","Umair"];
+  var lists=["Ali","Saad","Umair","waleed"];
+  var age=["20","32","12","11"];
+  var lst=["https://media.gettyimages.com/photos/quaideazam-picture-id184944186?s=612x612",
+  "https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0="];
+  
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: lists.length,
+      // body: ListView.builder(
+      //   itemCount: lists.length,
 
-        itemBuilder: (context,index){
-          return ListTile(
-            title:Text(lists[index])
-          );
-        }
-        ),
+      //   itemBuilder: (context,index){
+      //     return ListTile(
+      //       title:Text(lists[index]),
+      //       subtitle: Text(age[index]),
+      //     );
+      //   }
+      //   ),
       
+
+
+
+
+      // body: ListView.builder(
+      //   itemCount: lst.length,
+      //   itemBuilder: (context,index){
+      //     return Container(
+      //       height: 200,
+      //       width: 200,
+      //       child:Image.network(lst[index])
+      //     );
+      //   }),
+
+
+
+
+
+      // body: GridView.count(
+      //   crossAxisCount: 4 ,// one line mei kitne element(widgets)
+      //   crossAxisSpacing: 20,// left  to right spacing (horizontal)
+      //   mainAxisSpacing: 40, //vertical spacing
+      //   children: List.generate(lists.length, (index){
+      //     return Container(
+      //       color: Colors.yellow,
+      //       child: Center(child: Text(lists[index])),
+      //     );
+      //   })
+      //   )
+
+
+      appBar: AppBar(title: Text('Drawer Example'),),
+      drawer: Drawer(
+        child:ListView(children: [
+            DrawerHeader(
+            //   child: Container(
+            //   color:Colors.red
+            // ),
+            child: Text('ASD')),
+            ListTile(title:Text('Hello-1')),
+            ListTile(title:Text('Hello-2'))
+          ],
+
+        ) ,
+        ),
+    
     );
   }
 }
