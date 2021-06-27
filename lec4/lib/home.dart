d192c0d192437de40c2bce02f65c4c20dfd0e3eb
mohsinali-Iot <mohsinali481997@gmail.com>
2021-06-27 13:19:26 +0500

    state

--- lec4/lib/home.dart
+++ lec4/lib/home.dart
@@ -9,28 +9,65 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  var text="";
+  one(){
+    setState(() {
+      text="1";
+    });
+  }
+  two(){
+    setState(() {
+      text="2";
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 2, child:
-     Scaffold(
-       appBar:AppBar(
-         title: Text('Hello'),
-         bottom: TabBar(
-           unselectedLabelColor: Colors.grey,
-           labelColor: Colors.red,
-           indicatorColor: Colors.red,
+    // return DefaultTabController(length: 2, child:
+    //  Scaffold(
+    //    appBar:AppBar(
+    //      title: Text('Hello'),
+    //      bottom: TabBar(
+    //        unselectedLabelColor: Colors.grey,
+    //        labelColor: Colors.red,
+    //        indicatorColor: Colors.red,
            
            
-           tabs: [
-           Tab(child:Text('Login')),
-           Tab(child:Text('Register'))
-         ]),
-       ),
-       body: TabBarView(children: [
-         Login(),
-         Register()
+    //        tabs: [
+    //        Tab(child:Text('Login')),
+    //        Tab(child:Text('Register'))
+    //      ]),
+    //    ),
+    //    body: TabBarView(children: [
+    //      Login(),
+    //      Register()
 
-       ],)
-        ,)
-       );
+    //    ],)
+    //     ,)
+    //    );
+
+
+
+
+
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            Text('Text: $text'),
+            SizedBox(height:20),
+            ElevatedButton(onPressed: one, child:Text('Make-1')),
+            SizedBox(height:20),
+            
+            ElevatedButton(onPressed: two, child:Text('Make-2'))
+
+          ]),
+      )
+    );
+
   }}
